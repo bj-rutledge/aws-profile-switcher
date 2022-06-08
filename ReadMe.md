e937825499ee6ab7a1d32e318364a80ebddaa6c3
BJ Rutledge <billy.rutledge867037@gmail.com>
2022-06-08 09:25:43 -0700

    Created PowersShell script. Updated documentation to reflect.

--- ReadMe.md
+++ ReadMe.md
@@ -5,16 +5,33 @@
 ## Description 
 This scipt makes switching AWS CLI profiles simple and easy. 
 
-## Load this script in your .bashrc script. 
+## Included 
+Script for BASH and PowerShell
 
-### Example:
+## Notes: 
+To install in powershell, place the script in ~\Documents\WindowsPowerShell directory. 
+If your execution policy is strict, you will need to run: 
+````ps1
+Unblock-File -Path ~\Documents\WindowsPowerShell\aws-profile-switcher.ps1
+````
+If you do not have the AWS CLI installed on your windows machine, you can install it with the following command:
+````ps1
+Invoke-WebRequest -Uri https://awscli.amazonaws.com/AWSCLIV2.msi -OutFile $env:TEMP\AWSCLIV2.msi; Start-Process -FilePath $env:TEMP\AWSCLIV2.msi
+````
+or visit: 
+https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html 
+Happy Coding!!!
+
+## Load this script in your .bashrc script. 
+# BASH Examples. 
+### Examples:
 ```SH 
 # load aws profile switcher script 
 . ~/.aws-profile-switch 
 ```
 
 
-### Example:
+### BASH Examples:
 ```SH 
 
 # Switch to 'default' profile
@@ -33,3 +50,22 @@ awsprofile MyProfile
 ```
 
 
+### PowerShell Examples: 
+
+````ps1
+
+# Switch to 'default' profile
+Switch-AWSProfile -ProfileName 1 (short Switch-AWSProfile 1)
+
+# Switch to 'personal' profile
+Switch-AWSProfile -ProfileName 2 (short Switch-AWSProfile 2)
+
+# Switch profiles with prompt 
+Switch-AWSProfile 
+cmdlet Switch-AWSProfile at command pipeline position 1
+Supply values for the following parameters:
+(Type !? for Help.)
+ProfileName: MyProfile
+
+
+````
---
+++ aws-profile-switch.ps1
@@ -0,0 +1,68 @@
+# Created by BJ Rutledge 
+# LICENSE MIT
+# 6/8/2022
+
+# This script allows users to easily switch aws profiles. 
+# Example: 
+
+# Switch to 'default' profile
+# > Switch-AWSProfile -ProfileName 1 (short Switch-AWSProfile 1)
+
+# Switch to 'personal' profile
+# > Switch-AWSProfile -ProfileName 2 (short Switch-AWSProfile 2)
+
+# Switch profiles with prompt 
+# > Switch-AWSProfile 
+# MyProfile
+
+# Set aws profile 
+function Switch-AWSProfile {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage='Enter the profile name')]
+        [string]$ProfileName
+    )
+    
+    begin {
+        try {
+            Write-Host 'AWS CLI Installation:'
+            aws --version
+        }
+        catch {
+            {
+                1:  Error. It appears that you do not have the aws cli installed. 
+                    
+                2:  To install, visit https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html 
+                    or copy the following line into the shell: 
+                    
+                    Invoke-WebRequest -Uri https://awscli.amazonaws.com/AWSCLIV2.msi -OutFile $env:TEMP\AWSCLIV2.msi; Start-Process -FilePath $env:TEMP\AWSCLIV2.msi
+                    
+                    You may need to restart the shell after install. 
+                    Note: If you are using VsCode Intgrated shell, you may have some issues with AWS-CLI. But, it works fine in the regualr shell. 
+            }
+            break 
+        }
+    }
+    
+    process {
+
+        switch ($ProfileName) {
+            1 { 
+                # change this to whatever your default profile is named.
+                $ProfileName = 'default' 
+            }
+
+            2 {
+                # change this to whatever your personal profile is named
+                $ProfileName = 'personal'
+            }
+            # ... Add any other conditions you wish to switch on. 
+            Default {}
+        }
+        Set-Variable -Name AWS_PROFILE -Value $ProfileName -Scope Global 
+    }
+    
+    end {
+        Write-Host 'AWS CLI profile set to: ' $ProfileName
+    }
+}
